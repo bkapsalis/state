d544a858a39d596adbf0b7e454eaf43bc8847ac6
Bill Kapsalis <bkapsalis@gmail.com>
2023-02-27 18:37:13 -0800

    deploying

---
+++ client/build/precache-manifest.0005a6f9f23eb2642e159c25a4bf7292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bbb582fa5b7a2295c381845efb7bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6410f27c9d4fb01addf3",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "0a464bf37370e4cc55e9",
+    "url": "/static/js/2.47ffe161.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.47ffe161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6410f27c9d4fb01addf3",
+    "url": "/static/js/main.cfd3ee30.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cfd3ee30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
